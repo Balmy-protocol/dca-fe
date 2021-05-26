c2c6b03a569ce6379d8c781645a94ef004325ef0
Sebastian Torres <seba.j.torres@gmail.com>
2021-05-25 22:32:41 -0300

    feat(*): add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 // Declaring type of props - see "Typing Component Props" for more examples
 type AppProps = {
@@ -6,5 +6,16 @@ type AppProps = {
 }; /* use `interface` if exporting so that consumers can extend */
 
 // Easiest way to declare a Function Component; return type is inferred.
-const App : React.FunctionComponent<AppProps> = ({ message = 'Hello World' }: AppProps) => <div>{message}</div>;
+const App: React.FunctionComponent<AppProps> = ({ message = 'Hello World' }: AppProps) => <div>{message}</div>;
+
+var something = 'ble';
+
+var something = 'ble';
+
+var something = 'ble';
+
+var something = 'ble';
+
+var something = 'ble';
+
 export default App;
