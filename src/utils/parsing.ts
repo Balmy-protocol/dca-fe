e89e80940e4fd1e2d6ada0d6c90d859f79a3a697
Nicolas Chamo <nicolas@chamo.com.ar>
2021-08-16 16:36:00 -0300

    fix: use correct seconds in month

--- src/utils/parsing.ts
+++ src/utils/parsing.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from 'ethers';
 
-const HOURS_IN_MONTH = BigNumber.from(732);
+const HOURS_IN_MONTH = BigNumber.from(720);
 const DAYS_IN_WEEK = BigNumber.from(7);
 const HOURS_IN_DAYS = BigNumber.from(24);
 const MINUTES_IN_HOURS = BigNumber.from(60);
