5322d1279ded5455ed1f80eba192018b9cc19093
FiboApe <fiboape@gmail.com>
2022-06-08 18:20:02 -0300

    feat: disable low liquidity tokens

--- src/hooks/useTokenList.ts
+++ src/hooks/useTokenList.ts
@@ -6,6 +6,8 @@ import { getProtocolToken, PROTOCOL_TOKEN_ADDRESS } from 'mocks/tokens';
 import { useTokensLists } from 'state/token-lists/hooks';
 import useCurrentNetwork from './useCurrentNetwork';
 
+const BLACKLIST = ['0x5fe2b58c013d7601147dcdd68c143a77499f5531', '0x50b728d8d964fd00c2d0aad81718b71311fef68a'];
+
 function useTokenList(filter = true) {
   const currentNetwork = useCurrentNetwork();
   const tokensLists = useTokensLists();
@@ -21,7 +23,10 @@ function useTokenList(filter = true) {
                 ...acc,
                 ...keyBy(
                   tokensList.tokens.filter(
-                    (token) => token.chainId === currentNetwork.chainId && !Object.keys(acc).includes(token.address)
+                    (token) =>
+                      token.chainId === currentNetwork.chainId &&
+                      !Object.keys(acc).includes(token.address) &&
+                      (!filter || !BLACKLIST.includes(token.address))
                   ),
                   'address'
                 ),
