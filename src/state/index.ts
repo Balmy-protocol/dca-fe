b99fe9e4e905c8e1f08472f93eb5f367e0a6df00
FiboApe <fiboape@gmail.com>
2022-08-18 14:49:55 -0300

    feat: upgrade version of state

--- src/state/index.ts
+++ src/state/index.ts
@@ -15,7 +15,7 @@ import tokenLists, { getDefaultByUrl } from './token-lists/reducer';
 import config from './config/reducer';
 import error from './error/reducer';
 
-const LATEST_VERSION = '1.0.0';
+const LATEST_VERSION = '1.0.1';
 const LATEST_TRANSACTION_VERSION = '1.0.0';
 const TRANSACTION_VERSION_KEY = 'transactions_version';
 const TRANSACTION_KEY = 'redux_localstorage_simple_transactions';
