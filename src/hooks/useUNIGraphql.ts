d63da073b76177e07b4ef168dbebdca9c857d8b2
FiboApe <fiboape@gmail.com>
2022-08-30 10:54:38 -0300

    fix: some fixes for defaults position versions

--- src/hooks/useUNIGraphql.ts
+++ src/hooks/useUNIGraphql.ts
@@ -1,8 +1,8 @@
-import { POSITION_VERSION_3, PositionVersions } from 'config';
+import { PositionVersions, LATEST_VERSION } from 'config';
 import useCurrentNetwork from './useCurrentNetwork';
 import useWeb3Service from './useWeb3Service';
 
-function useUNIGraphql(chainId?: number, version: PositionVersions = POSITION_VERSION_3) {
+function useUNIGraphql(chainId?: number, version: PositionVersions = LATEST_VERSION) {
   const web3Service = useWeb3Service();
   const currentNetwork = useCurrentNetwork();
   const chainIdTouse = chainId || currentNetwork.chainId;
