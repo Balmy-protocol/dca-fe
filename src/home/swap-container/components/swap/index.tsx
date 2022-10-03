fd36f16d03ef1015cecc5e81289203074fb41445
FiboApe <fiboape@gmail.com>
2022-10-03 16:05:17 -0300

    feat(positionservice): correctly add yield to newly created positions

--- src/home/swap-container/components/swap/index.tsx
+++ src/home/swap-container/components/swap/index.tsx
@@ -301,6 +301,8 @@ const Swap = ({
         typeData: {
           from,
           to,
+          fromYield: fromYield?.tokenAddress,
+          toYield: toYield?.tokenAddress,
           fromValue,
           frequencyType: frequencyType.toString(),
           frequencyValue,
