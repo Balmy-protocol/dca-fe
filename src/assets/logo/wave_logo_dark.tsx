114e51b472fada41aa1110d2ade473e1765e0edd
FiboApe <fiboape@gmail.com>
2023-01-23 14:07:25 -0300

    feat(navbar): make logo go to main page

--- src/assets/logo/wave_logo_dark.tsx
+++ src/assets/logo/wave_logo_dark.tsx
@@ -4,12 +4,20 @@ import WaveLogoDark from './wave_logo_dark.svg';
 
 interface WaveLogoDarkProps {
   size?: string;
+  onClick?: () => void;
 }
 
-const WhaveLogoDark = ({ size }: WaveLogoDarkProps) => {
+const WhaveLogoDark = ({ size, onClick }: WaveLogoDarkProps) => {
   const realSize = size || '28px';
   return (
-    <SvgIcon component={WaveLogoDark} viewBox="0 0 315 140" fill="none" style={{ fontSize: realSize, fill: 'none' }} />
+    <SvgIcon
+      component={WaveLogoDark}
+      sx={{ ...(onClick ? { cursor: 'pointer' } : {}) }}
+      onClick={onClick}
+      viewBox="0 0 315 140"
+      fill="none"
+      style={{ fontSize: realSize, fill: 'none' }}
+    />
   );
 };
 
