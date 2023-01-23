f7db06a27279c0ba0f3054b6b107d89e0b278efa
FiboApe <fiboape@gmail.com>
2023-01-23 11:17:16 -0300

    feat(*): disable jEUR yield

--- src/config/constants/common.ts
+++ src/config/constants/common.ts
@@ -42,4 +42,5 @@ export const TOKEN_BLACKLIST = [
   '0x50b728d8d964fd00c2d0aad81718b71311fef68a', // POLY - SNX
   '0x65559aa14915a70190438ef90104769e5e890a00', // OE - ENS
   '0x289ba1701c2f088cf0faf8b3705246331cb8a839', // ARBI - LPT. Disabled due to liquidity decrease
+  '0x1dd5629903441b2dd0d03f76ec7673add920e765', // POLY - jEUR. Disabled due to aave not supporting anymore
 ];
