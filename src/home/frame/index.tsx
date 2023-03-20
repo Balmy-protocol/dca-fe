deed4d9be12daddadbff0f708330fa4b22e593d8
FiboApe <fiboape@gmail.com>
2023-03-20 17:02:23 -0300

    fix(home/frame): set correct dependencies for useEffect hook on network change

--- src/home/frame/index.tsx
+++ src/home/frame/index.tsx
@@ -37,16 +37,18 @@ const HomeFrame = ({ isLoading }: HomeFrameProps) => {
   // const hasInitiallySetNetwork = React.useState()
 
   React.useEffect(() => {
-    if (SUPPORTED_NETWORKS_DCA.includes(Number(chainId))) {
-      dispatch(setDCAChainId(Number(chainId)));
+    const chainIdToUse = Number(chainId);
+
+    if (SUPPORTED_NETWORKS_DCA.includes(chainIdToUse)) {
+      dispatch(setDCAChainId(chainIdToUse));
       // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      dispatch(fetchGraphTokenList(Number(chainId)));
+      dispatch(fetchGraphTokenList(chainIdToUse));
     } else if (SUPPORTED_NETWORKS_DCA.includes(currentNetwork.chainId)) {
       dispatch(setDCAChainId(currentNetwork.chainId));
     } else {
       dispatch(setDCAChainId(DEFAULT_NETWORK_FOR_VERSION[POSITION_VERSION_4].chainId));
     }
-  }, [chainId, currentNetwork.chainId]);
+  }, []);
 
   React.useEffect(() => {
     const fetchPairs = async () => {
@@ -70,8 +72,11 @@ const HomeFrame = ({ isLoading }: HomeFrameProps) => {
       replaceHistory(`/create/${newChainId}`);
       dispatch(setDCAChainId(newChainId));
       setHasLoadedPairs(false);
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      dispatch(fetchGraphTokenList(newChainId));
     }
   };
+
   const { loading: isLoadingSwapIntervals, data: swapIntervalsData } = useQuery<GetSwapIntervalsGraphqlResponse>(
     getAvailableIntervals,
     {
