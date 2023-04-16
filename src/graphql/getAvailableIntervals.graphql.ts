2c41a9a86e1422ad68103dc00a6b5f0087f9fb0a
FiboApe <fiboape@gmail.com>
2023-04-16 12:05:07 -0300

    fix(graphql): fix graphql query

--- src/graphql/getAvailableIntervals.graphql.ts
+++ src/graphql/getAvailableIntervals.graphql.ts
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 const getAvailableIntervals = gql`
-  query getAvailableIntervals($subgraphError: Boolean) {
+  query getAvailableIntervals($subgraphError: String) {
     swapIntervals(where: { active: true }, subgraphError: $subgraphError) {
       id
       interval
