6b0c1bb2afdbf93cec0cf4a53210dc35ee6e27a5
Fiboape <94012134+FiboApe@users.noreply.github.com>
2023-05-24 18:01:05 -0300

    test(simulationservice): add simulationService unit tests (#402)
    * test(simulationservice): add simulationService unit tests
    
    * test(yieldservice): add yield service tests (#406)

--- src/services/yieldService.ts
+++ src/services/yieldService.ts
@@ -5,7 +5,6 @@ import { DefillamaResponse, YieldOptions } from '@types';
 // MOCKS
 import { ALLOWED_YIELDS, DISABLED_YIELDS } from '@constants';
 import { getProtocolToken, getWrappedProtocolToken } from '@common/mocks/tokens';
-import WalletService from './walletService';
 import ProviderService from './providerService';
 
 export default class YieldService {
@@ -13,10 +12,7 @@ export default class YieldService {
 
   providerService: ProviderService;
 
-  walletService: WalletService;
-
-  constructor(walletService: WalletService, providerService: ProviderService, axiosClient: AxiosInstance) {
-    this.walletService = walletService;
+  constructor(providerService: ProviderService, axiosClient: AxiosInstance) {
     this.providerService = providerService;
     this.axiosClient = axiosClient;
   }
@@ -44,7 +40,7 @@ export default class YieldService {
 
       const enabledTokens = foundYield?.underlyingTokens?.map((token) => token.toLowerCase()) || [];
 
-      const forcedEnabledTokens = baseYield.forcedUnderlyings || [];
+      const forcedEnabledTokens = baseYield.forcedUnderlyings?.map((underlying) => underlying.toLowerCase()) || [];
 
       const wrappedProtocolToken = getWrappedProtocolToken(chainidTouse);
 
