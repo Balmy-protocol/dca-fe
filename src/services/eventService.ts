1df4e10657860a222f2876d4bbfee986a5b313a8
Ged <0xged@protonmail.com>
2023-06-06 18:14:03 -0300

    ci: use secrets and environments to build

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -11,7 +11,7 @@ export default class EventService {
 
   constructor(providerService: ProviderService) {
     this.providerService = providerService;
-    this.mixpanel = MixpanelLibray.init('7f62e8855fcbcd72de460ba40b6fc06b', { api_host: MEAN_PROXY_PANEL_URL }, ' ');
+    this.mixpanel = MixpanelLibray.init(process.env.MIXPANEL_TOKEN!, { api_host: MEAN_PROXY_PANEL_URL }, ' ');
     this.mixpanel.set_config({ persistence: 'localStorage', ignore_dnt: true });
   }
 
@@ -24,6 +24,7 @@ export default class EventService {
     const network = await this.providerService.getNetwork();
     const foundNetwork = find(NETWORKS, { chainId: network.chainId });
     return new Promise<void>((resolve, reject) => {
+      if (!this.mixpanel) return resolve();
       this.mixpanel.track(
         action,
         {
