4a2bc52b3b318dc41ed69c9ca3aa71e4e96b3d37
FiboApe <fiboape@gmail.com>
2023-06-20 17:50:23 -0300

    feat(*): re-enable changelly

--- src/constants/aggregator.ts
+++ src/constants/aggregator.ts
@@ -34,7 +34,7 @@ export const DEFAULT_AGGREGATOR_SETTINGS: {
 } = {
   slippage: 0.3,
   gasSpeed: GAS_KEY_AVERAGE,
-  disabledDexes: ['firebird', 'changelly'],
+  disabledDexes: ['firebird'],
   showTransactionCost: true,
   confetti: 100,
   sorting: SORT_MOST_RETURN,
--- src/state/index.ts
+++ src/state/index.ts
@@ -22,7 +22,7 @@ import config from './config/reducer';
 import error from './error/reducer';
 
 const LATEST_VERSION = '1.0.6';
-const LATEST_AGGREGATOR_SETTINGS_VERSION = '1.0.0';
+const LATEST_AGGREGATOR_SETTINGS_VERSION = '1.0.1';
 const LATEST_TRANSACTION_VERSION = '1.0.0';
 const TRANSACTION_VERSION_KEY = 'transactions_version';
 const TRANSACTION_KEY = 'redux_localstorage_simple_transactions';
