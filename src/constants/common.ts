94a34e3cb1fd7a0fbfb4323ee3b4fc4c4f6d34c8
Ged <0xged@protonmail.com>
2023-08-03 11:24:05 -0300

    chore: adds reth to blacklist

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -52,47 +52,49 @@ export const TRANSACTION_ACTION_TYPES: Record<TransactionActionType, Transaction
 };
 
 export const TOKEN_BLACKLIST = [
-  '0x2e9a6df78e42a30712c10a9dc4b1c8656f8f2879', // ARBITRUM - Malicious MKR
-  '0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000', // OP - WTF optimism
+  '0x2e9a6df78e42a30712c10a9dc4b1c8656f8f2879', // Arbitrum - Malicious MKR
+  '0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000', // Optimism - WTF optimism
 ];
 
 export const DCA_PAIR_BLACKLIST = [
-  '0xa7a7ffe0520e90491e58c9c77f78d7cfc32d019e-0xd125443f38a69d776177c2b9c041f462936f8218', // POLY - FBX/waWETH
-  '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619-0xd125443f38a69d776177c2b9c041f462936f8218', // POLY - FBX/WETH
+  '0xa7a7ffe0520e90491e58c9c77f78d7cfc32d019e-0xd125443f38a69d776177c2b9c041f462936f8218', // Polygon - FBX/waWETH
+  '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619-0xd125443f38a69d776177c2b9c041f462936f8218', // Polygon - FBX/WETH
 ];
 
 export const DCA_TOKEN_BLACKLIST = [
-  '0x5fe2b58c013d7601147dcdd68c143a77499f5531', // POLY - GRT
-  '0x50b728d8d964fd00c2d0aad81718b71311fef68a', // POLY - SNX
-  '0x65559aa14915a70190438ef90104769e5e890a00', // OE - ENS
-  '0x289ba1701c2f088cf0faf8b3705246331cb8a839', // ARBI - LPT. Disabled due to liquidity decrease
-  '0x1dd5629903441b2dd0d03f76ec7673add920e765', // POLY - jEUR. Disabled due to aave not supporting anymore
-  '0x5d47baba0d66083c52009271faf3f50dcc01023c', // OP - UNIDEX. Disabled due to liquidity moved to velodrome
-  '0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000', // OP - WTF optimism
-  '0x2760e46d9bb43dafcbecaad1f64b93207f9f0ed7', // POLY - MVX
+  '0x5fe2b58c013d7601147dcdd68c143a77499f5531', // Polygon - GRT
+  '0x50b728d8d964fd00c2d0aad81718b71311fef68a', // Polygon - SNX
+  '0x65559aa14915a70190438ef90104769e5e890a00', // Optimism - ENS
+  '0x289ba1701c2f088cf0faf8b3705246331cb8a839', // Arbitrum - LPT. Disabled due to liquidity decrease
+  '0x1dd5629903441b2dd0d03f76ec7673add920e765', // Polygon - jEUR. Disabled due to aave not supporting anymore
+  '0x5d47baba0d66083c52009271faf3f50dcc01023c', // Optimism - UNIDEX. Disabled due to liquidity moved to velodrome
+  '0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000', // Optimism - WTF optimism
+  '0x2760e46d9bb43dafcbecaad1f64b93207f9f0ed7', // Polygon - MVX
   // TODO: Remove this once we check beefy works correctly
-  '0xf0cbbfad265a0e7c7e4fd2e1e6027a8dfa25676b', // OP - WBTC Beefy wrapper
-  '0x72b25ce2f946c95a2194f5ac3322443d0057bc94', // OP - DAI Beefy wrapper
-  '0x185d3a08140efaeb3c6bf173e751afb0bcb0d0c6', // OP - USDC Beefy wrapper
-  '0xaab6af05e12faae0a5d9597c79588846f0df15b8', // POLY - MVX optimism Beefy wrapper
-  '0xe0b52e49357fd4daf2c15e02058dce6bc0057db4', // POLY - AGEUR
+  '0xf0cbbfad265a0e7c7e4fd2e1e6027a8dfa25676b', // Optimism - WBTC Beefy wrapper
+  '0x72b25ce2f946c95a2194f5ac3322443d0057bc94', // Optimism - DAI Beefy wrapper
+  '0x185d3a08140efaeb3c6bf173e751afb0bcb0d0c6', // Optimism - USDC Beefy wrapper
+  '0xaab6af05e12faae0a5d9597c79588846f0df15b8', // Polygon - MVX optimism Beefy wrapper
+  '0xe0b52e49357fd4daf2c15e02058dce6bc0057db4', // Polygon - AGEUR
 
-  '0xcd0e5871c97c663d43c62b5049c123bb45bfe2cc', // ETH - USDC. Euler. Disabled due to hack.
-  '0xd4de9d2fc1607d1df63e1c95ecbfa8d7946f5457', // ETH - WETH. Euler. Disabled due to hack.
-  '0xc4113b7605d691e073c162809060b6c5ae402f1e', // ETH - DAI. Euler. Disabled due to hack.
-  '0x48e345cb84895eab4db4c44ff9b619ca0be671d9', // ETH - WBTC. Euler. Disabled due to hack.
-  '0xb95e6eee428902c234855990e18a632fa34407dc', // ETH - LUSD. Euler. Disabled due to hack.
-  '0x7c6d161b367ec0605260628c37b8dd778446256b', // ETH - wstETH. Euler. Disabled due to hack.
+  '0xcd0e5871c97c663d43c62b5049c123bb45bfe2cc', // Ethereum - USDC. Euler. Disabled due to hack.
+  '0xd4de9d2fc1607d1df63e1c95ecbfa8d7946f5457', // Ethereum - WETH. Euler. Disabled due to hack.
+  '0xc4113b7605d691e073c162809060b6c5ae402f1e', // Ethereum - DAI. Euler. Disabled due to hack.
+  '0x48e345cb84895eab4db4c44ff9b619ca0be671d9', // Ethereum - WBTC. Euler. Disabled due to hack.
+  '0xb95e6eee428902c234855990e18a632fa34407dc', // Ethereum - LUSD. Euler. Disabled due to hack.
+  '0x7c6d161b367ec0605260628c37b8dd778446256b', // Ethereum - wstETH. Euler. Disabled due to hack.
 
-  '0x57f93e3fcb2b7ef126aeeda1f959c9ee4625aa70', // OP - MAI Aave v3 - missing defi llama id
-  '0x348a1213fa28a43855e5bb103bdfd7f357543626', // OP - OP Aave v3
-  '0x1622bf67e6e5747b81866fe0b85178a93c7f86e3', // ARBI - UMAMI
-  '0x1bbc5845e7e2000938c840f0343a975269c348ef', // Yearn - WBTC
-  '0x296f55f8fb28e498b858d0bcda06d955b2cb3f97', // OP - STG
-  '0x18c46621af7cbcdf58b155517d22b4ae7c5f2cd1', // POLY - wstEth Aave
-  '0xf2f77fe7b8e66571e0fca7104c4d670bf1c8d722', // POLY - jBRL
-  '0xbd1fe73e1f12bd2bc237de9b626f056f21f86427', // POLY - jMXN
-  '0x250632378e573c6be1ac2f97fcdf00515d0aa91b', // BSC - BETH
+  '0xae78736cd615f374d3085123a210448e74fc6393', // Ethereum - rETH. Disabled until being tested and released
+
+  '0x57f93e3fcb2b7ef126aeeda1f959c9ee4625aa70', // Optimism - MAI Aave v3 - missing defi llama id
+  '0x348a1213fa28a43855e5bb103bdfd7f357543626', // Optimism - OP Aave v3
+  '0x1622bf67e6e5747b81866fe0b85178a93c7f86e3', // Arbitrum - UMAMI
+  '0x1bbc5845e7e2000938c840f0343a975269c348ef', // Optimism - Yearn - WBTC
+  '0x296f55f8fb28e498b858d0bcda06d955b2cb3f97', // Optimism - STG
+  '0x18c46621af7cbcdf58b155517d22b4ae7c5f2cd1', // Polygon - wstEth Aave
+  '0xf2f77fe7b8e66571e0fca7104c4d670bf1c8d722', // Polygon - jBRL
+  '0xbd1fe73e1f12bd2bc237de9b626f056f21f86427', // Polygon - jMXN
+  '0x250632378e573c6be1ac2f97fcdf00515d0aa91b', // BNB Chain - BETH
 
   // Arbitrum - GMD - Blacklisting so we can test it out
   '0x4945970efeec98d393b4b979b9be265a3ae28a8b',
