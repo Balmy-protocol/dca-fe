f3e03d21c8979bd224259ef2a8ae69b18dcca6f1
Ged <0xged@protonmail.com>
2023-08-10 14:03:41 -0300

    chore: remove aave mai from blacklist

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -88,7 +88,6 @@ export const DCA_TOKEN_BLACKLIST = [
   '0xb95e6eee428902c234855990e18a632fa34407dc', // Ethereum - LUSD. Euler. Disabled due to hack.
   '0x7c6d161b367ec0605260628c37b8dd778446256b', // Ethereum - wstETH. Euler. Disabled due to hack.
 
-  '0x57f93e3fcb2b7ef126aeeda1f959c9ee4625aa70', // Optimism - MAI Aave v3 - missing defi llama id
   '0x348a1213fa28a43855e5bb103bdfd7f357543626', // Optimism - OP Aave v3
   '0x1622bf67e6e5747b81866fe0b85178a93c7f86e3', // Arbitrum - UMAMI
   '0x1bbc5845e7e2000938c840f0343a975269c348ef', // Optimism - Yearn - WBTC
