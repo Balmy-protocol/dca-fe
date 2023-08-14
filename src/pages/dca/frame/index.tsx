185ca9c100cb50594842cb15cc63e1c24550b8e9
Fiboape <94012134+FiboApe@users.noreply.github.com>
2023-08-13 21:07:27 -0300

    feat(*): add subgraph warning about downtime (#433)

--- src/pages/dca/frame/index.tsx
+++ src/pages/dca/frame/index.tsx
@@ -27,6 +27,8 @@ import useSelectedNetwork from '@hooks/useSelectedNetwork';
 import useSdkMappedChains from '@hooks/useMappedSdkChains';
 import useWalletService from '@hooks/useWalletService';
 import useWeb3Service from '@hooks/useWeb3Service';
+import { FormattedMessage } from 'react-intl';
+import Alert from '@mui/material/Alert';
 import { fetchGraphTokenList } from '@state/token-lists/actions';
 import CreatePosition from '../create-position';
 import Positions from '../positions';
@@ -129,6 +131,14 @@ const HomeFrame = ({ isLoading }: HomeFrameProps) => {
 
   return (
     <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Alert severity="warning">
+          <FormattedMessage
+            description="theGraphDowntime"
+            defaultMessage="Our subgraph provider will go under maintenance on August 24 at 07:00 UTC. There might be some issues loading positions during a small period of the maintenance window. Please note that positions are safe at all times and service will return to normal shortly"
+          />
+        </Alert>
+      </Grid>
       {isLoadingIntervals ? (
         <Grid item xs={12} style={{ display: 'flex' }}>
           <CenteredLoadingIndicator size={70} />
