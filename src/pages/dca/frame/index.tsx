381da1731e564851b6e4c0b44132d3f6139cfcba
Sam Bugs <sambugs@protonmail.com>
2023-09-07 14:12:40 -0300

    Revert "feat(*): add subgraph warning about it not working"
    This reverts commit 279d56d77efdea1fb527eee0335003f7e01f3d69.

--- src/pages/dca/frame/index.tsx
+++ src/pages/dca/frame/index.tsx
@@ -27,8 +27,6 @@ import useSelectedNetwork from '@hooks/useSelectedNetwork';
 import useSdkMappedChains from '@hooks/useMappedSdkChains';
 import useWalletService from '@hooks/useWalletService';
 import useWeb3Service from '@hooks/useWeb3Service';
-import Alert from '@mui/material/Alert';
-import { FormattedMessage } from 'react-intl';
 import { fetchGraphTokenList } from '@state/token-lists/actions';
 import CreatePosition from '../create-position';
 import Positions from '../positions';
@@ -131,14 +129,6 @@ const HomeFrame = ({ isLoading }: HomeFrameProps) => {
 
   return (
     <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Alert severity="warning">
-          <FormattedMessage
-            description="theGraphError"
-            defaultMessage="There is currently a problem with our subgraph provider and some positions might not show. Please note that positions are safe at all times and service will return to normal shortly"
-          />
-        </Alert>
-      </Grid>
       {isLoadingIntervals ? (
         <Grid item xs={12} style={{ display: 'flex' }}>
           <CenteredLoadingIndicator size={70} />
