1dd67709a9e30cfb1dfa2db6cd979ff6988b555f
Ged <0xged@protonmail.com>
2023-09-09 12:48:37 -0300

    feat: adds 1inch via api and updates sdk

--- src/services/sdkService.ts
+++ src/services/sdkService.ts
@@ -70,7 +70,7 @@ export default class SdkService {
                     `${MEAN_API_URL}/v1/swap/networks/${chainId}/quotes/${sourceId}`,
                   sources: SOURCES_METADATA,
                 },
-                sourceIds: ['uniswap', 'rango', '0x', 'firebird', 'changelly'],
+                sourceIds: ['1inch', 'uniswap', 'rango', '0x', 'firebird', 'changelly'],
               },
             ],
           },
