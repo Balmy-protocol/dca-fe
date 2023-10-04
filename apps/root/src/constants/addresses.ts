f198299d56e075870002cef341e97c13dd745af7
Fiboape <94012134+FiboApe@users.noreply.github.com>
2023-10-04 13:44:55 -0300

    feat(addresses): update base-goerli subgraph url (#458)

--- apps/root/src/constants/addresses.ts
+++ apps/root/src/constants/addresses.ts
@@ -596,7 +596,7 @@ export const MEAN_GRAPHQL_URL: AddressMap<PositionVersions> = {
     [NETWORKS.arbitrum.chainId]: 'https://api.thegraph.com/subgraphs/name/mean-finance/dca-v2-yf-arbitrum',
     [NETWORKS.mainnet.chainId]: 'https://api.thegraph.com/subgraphs/name/mean-finance/dca-v2-yf-ethereum',
     [NETWORKS.bsc.chainId]: 'https://api.thegraph.com/subgraphs/name/mean-finance/dca-v2-yf-bnb',
-    [NETWORKS.baseGoerli.chainId]: 'https://api.studio.thegraph.com/proxy/35446/dca-v2-yf-base-goerli/v0.0.1/',
+    [NETWORKS.baseGoerli.chainId]: 'https://api.thegraph.com/subgraphs/name/mean-finance/dca-v2-yf-base-goerli',
   },
 };
 
