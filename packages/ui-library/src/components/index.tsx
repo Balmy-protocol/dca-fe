4da326134e47fd5319bbe9a3cfafceffc156bd29
th0rOdinson <th0r@balmy.xyz>
2023-10-04 00:57:08 -0300

    ui-library setup for templating and storybook for mui components

--- packages/ui-library/src/components/index.tsx
+++ packages/ui-library/src/components/index.tsx
@@ -1,1 +1,2 @@
+// component exports
 export * from './button';
--- packages/ui-library/turbo/generators/config.ts
+++ packages/ui-library/turbo/generators/config.ts
@@ -3,6 +3,10 @@ import type { PlopTypes } from '@turbo/gen';
 // Learn more about Turborepo Generators at https://turbo.build/repo/docs/core-concepts/monorepos/code-generation
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  plop.setHelper('downcase', (text) => {
+    return text.toLowerCase();
+  });
+
   // A simple generator to add a new React component to the internal UI library
   plop.setGenerator('react-component', {
     description: 'Adds a new react component',
@@ -23,6 +27,35 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
         type: 'add',
         path: 'src/components/{{downcase name}}/{{ pascalCase name }}.stories.tsx',
         templateFile: 'templates/component-story.hbs',
+      },
+      {
+        type: 'append',
+        path: 'src/components/index.tsx',
+        pattern: /(?<insertion>\/\/ component exports)/g,
+        template: 'export * from "./{{downcase name}}";',
+      },
+    ],
+  });
+
+  plop.setGenerator('mui-component', {
+    description: 'Adds a new MUI component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the component?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{downcase name}}/index.tsx',
+        templateFile: 'templates/mui-component.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{downcase name}}/{{ pascalCase name }}.stories.tsx',
+        templateFile: 'templates/mui-component-story.hbs',
       },
       {
         type: 'append',
