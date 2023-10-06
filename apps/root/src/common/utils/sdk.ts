aeaf5360b6ba7d85d929dc39b7b23398d98e77b2
th0rOdinson <th0r@balmy.xyz>
2023-10-06 17:17:34 -0300

    Tiny refactor over helper function

--- apps/root/src/common/utils/sdk.ts
+++ apps/root/src/common/utils/sdk.ts
@@ -1,12 +1,10 @@
 import { DCAPermission, DCAPermissionSet } from '@mean-finance/sdk';
 import { PermissionSet } from '@types';
 
-const permissionEnumValues = Object.values(DCAPermission);
-
 export const parsePermissionsForSdk = (permissionSets: PermissionSet[]): DCAPermissionSet[] => {
   return permissionSets.map((permissionSet) => {
     const { operator, permissions } = permissionSet;
-    const parsedPermissions = permissions.map((permissionIndex) => permissionEnumValues[permissionIndex]);
+    const parsedPermissions = permissions.map((permission) => Object.values(DCAPermission)[permission]);
     return { operator, permissions: parsedPermissions };
   });
 };
