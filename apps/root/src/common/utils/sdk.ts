39e5ad47a56ccbb4c565662b9696190351321ff4
th0rOdinson <th0r@balmy.xyz>
2023-10-06 19:00:40 -0300

    Added mapping for helper function

--- apps/root/src/common/utils/sdk.ts
+++ apps/root/src/common/utils/sdk.ts
@@ -1,10 +1,17 @@
 import { DCAPermission, DCAPermissionSet } from '@mean-finance/sdk';
 import { PermissionSet } from '@types';
 
+const permissionMapping: Record<number, DCAPermission> = {
+  0: DCAPermission.INCREASE,
+  1: DCAPermission.REDUCE,
+  2: DCAPermission.WITHDRAW,
+  3: DCAPermission.TERMINATE,
+};
+
 export const parsePermissionsForSdk = (permissionSets: PermissionSet[]): DCAPermissionSet[] => {
   return permissionSets.map((permissionSet) => {
     const { operator, permissions } = permissionSet;
-    const parsedPermissions = permissions.map((permission) => Object.values(DCAPermission)[permission]);
+    const parsedPermissions = permissions.map((permission) => permissionMapping[permission]);
     return { operator, permissions: parsedPermissions };
   });
 };
