a2c4eb1cb1f874091f8df7c9d32727baf69206ce
Fiboape <94012134+FiboApe@users.noreply.github.com>
2023-11-08 17:50:41 -0300

    fix(dca): remove USDT untill we can figure out whats going on with approvals (#491)

--- apps/root/src/constants/common.ts
+++ apps/root/src/constants/common.ts
@@ -144,6 +144,7 @@ export const DCA_TOKEN_BLACKLIST = [
   '0x7597f9b2944809de4e5a841d6fa5e499099a5a42', // Arbitrum - MAI on Aave - Hiding because $MAI lost support on Aave
 
   '0xb5c064f955d8e7f38fe0460c556a72987494ee17', // Polygon - QUICK - Hiding until release
+  '0xdac17f958d2ee523a2206206994597c13d831ec7', // Ethereum - USDT - Until we can figure out whats going on with approval
 
   ...SHAME_LIST,
 ].map((a) => a.toLowerCase());
