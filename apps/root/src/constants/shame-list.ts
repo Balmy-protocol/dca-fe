5c7fd34a83a31311593dbdc4a837351ffd601e87
FiboApe <fiboape@gmail.com>
2024-01-04 11:21:47 -0300

    feat(*): disable aave, again

--- apps/root/src/constants/shame-list.ts
+++ apps/root/src/constants/shame-list.ts
@@ -61,6 +61,9 @@
 .......:*#%##=..-+:..-=::*%=..:##-*%::*#-.:#+::#####:..............................
 **/
 
-export const AAVE_FROZEN_TOKENS: string[] = [];
+export const AAVE_FROZEN_TOKENS: string[] = [
+  '0xfa68fb4628dff1028cfec22b4162fccd0d45efb6', // aave-v3 maticx again due to vuln
+  '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', // aave-v3 wmatic again due to vuln
+];
 
 export const SHAME_LIST = [...AAVE_FROZEN_TOKENS];
