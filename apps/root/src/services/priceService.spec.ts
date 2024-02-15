2c3ea8859dfd204b1d4cb44abf4b2c7440c1a84e
Ged <97693615+0xged@users.noreply.github.com>
2024-02-15 11:24:46 -0300

    chore: updates sdk (#568)

--- apps/root/src/services/priceService.spec.ts
+++ apps/root/src/services/priceService.spec.ts
@@ -48,6 +48,7 @@ describe('Price Service', () => {
       priceService: {
         getHistoricalPricesForChain: mockGetHistoricalPricesForChain,
         supportedChains: jest.fn(),
+        getChart: jest.fn(),
         supportedQueries: jest.fn(),
         getCurrentPricesForChain: mockGetCurrentPricesForChain,
         getCurrentPrices: jest.fn(),
