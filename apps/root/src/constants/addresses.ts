04be9c403c63659d21043b7cc28ee68bcb7e95f2
Ged <0xged@protonmail.com>
2024-02-15 16:11:57 -0300

    chore: remove metis from aggregator until release

--- apps/root/src/constants/addresses.ts
+++ apps/root/src/constants/addresses.ts
@@ -737,7 +737,7 @@ export const ZRX_API_ADDRESS: Record<number, string> = {
   [NETWORKS.arbitrum.chainId]: 'https://arbitrum.api.0x.org',
 };
 
-export const REMOVED_AGG_CHAINS = [30, 58];
+export const REMOVED_AGG_CHAINS = [1088, 30, 58];
 
 export const getGhTokenListLogoUrl = (chainId: number, address: string) =>
   `https://raw.githubusercontent.com/Mean-Finance/token-list/main/assets/chains/${chainId}/${address.toLowerCase()}.svg`;
