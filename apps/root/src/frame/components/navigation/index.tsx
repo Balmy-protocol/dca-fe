d796f942ae44d23e4dc28537b647d9c289fc36e0
FiboApe <fiboape@gmail.com>
2024-03-28 11:44:58 -0300

    fix(*): bly-2036

--- apps/root/src/frame/components/navigation/index.tsx
+++ apps/root/src/frame/components/navigation/index.tsx
@@ -83,25 +83,18 @@ const Navigation = ({
   React.useEffect(() => {
     if (HOME_ROUTES.includes(location.pathname)) {
       dispatch(changeRoute('home'));
-      pushToHistory('/');
     } else if (location.pathname.startsWith('/history')) {
       dispatch(changeRoute('history'));
-      pushToHistory('/history');
     } else if (location.pathname.startsWith('/create')) {
       dispatch(changeRoute('create'));
-      pushToHistory('/create');
     } else if (location.pathname.startsWith('/positions')) {
       dispatch(changeRoute('positions'));
-      pushToHistory('/positions');
     } else if (location.pathname.startsWith('/swap')) {
       dispatch(changeRoute('swap'));
-      pushToHistory('/swap');
     } else if (location.pathname.startsWith('/transfer')) {
       dispatch(changeRoute('transfer'));
-      pushToHistory('/transfer');
     } else if (location.pathname.startsWith('/settings')) {
       dispatch(changeRoute('settings'));
-      pushToHistory('/settings');
     }
   }, []);
 
