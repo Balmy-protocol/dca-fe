0f5a29d2d637b30988e1b4d9b926ff09134ea506
th0rOdinson <th0r@balmy.xyz>
2024-03-29 16:49:01 -0300

    BLY-1994

--- packages/ui-library/src/emojis/index.tsx
+++ packages/ui-library/src/emojis/index.tsx
@@ -5,3 +5,5 @@ export { default as GrinningFaceWithBigEyesEmoji } from './grinning-face-with-bi
 export { default as SmilingFaceWithHeartEyesEmoji } from './smiling-face-with-heart-eyes';
 export { default as ThumbsUpEmoji } from './thumbs-up';
 export { default as ThumbsDownEmoji } from './thumbs-down';
+export { default as IndexPointingAtTheViewerEmoji } from './index-pointing-at-the-viewer';
+export { default as ManShruggingEmoji } from './man-shrugging';
