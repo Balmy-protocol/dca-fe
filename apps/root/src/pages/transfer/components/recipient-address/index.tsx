274e65ee084db9f16a03a3e6cd4e1c7c5b452f2f
FiboApe <fiboape@gmail.com>
2024-04-15 16:03:38 -0300

    fix(transfer): improve transfer url changes

--- apps/root/src/pages/transfer/components/recipient-address/index.tsx
+++ apps/root/src/pages/transfer/components/recipient-address/index.tsx
@@ -29,10 +29,6 @@ const RecipientAddress = () => {
     }
   };
 
-  React.useEffect(() => {
-    onRecipientChange(storedRecipient);
-  }, [storedRecipient]);
-
   return (
     <AddressInput
       id="recipientAddress"
