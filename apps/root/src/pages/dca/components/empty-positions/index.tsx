d6821e1315c701bb43ebac8703265e59b516dcae
FiboApe <fiboape@gmail.com>
2024-04-22 10:25:49 -0300

    fix(dca): fix empty positions list

--- apps/root/src/pages/dca/components/empty-positions/index.tsx
+++ apps/root/src/pages/dca/components/empty-positions/index.tsx
@@ -1,35 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FormattedMessage } from 'react-intl';
-import { Typography, Card } from 'ui-library';
+import { Typography, Card, colors, ContainerBox, Button } from 'ui-library';
+import { DCA_CREATE_ROUTE } from '@constants/routes';
+import usePushToHistory from '@hooks/usePushToHistory';
+import { useAppDispatch } from '@state/hooks';
+import { changeRoute } from '@state/tabs/actions';
 
 const StyledCard = styled(Card)`
-  margin: 10px;
-  position: relative;
-  min-height: 215px;
-  border: 3px dashed;
-  flex-grow: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  align-self: stretch;
+  ${({
+    theme: {
+      palette: { mode },
+      spacing,
+    },
+  }) => `
+    margin: ${spacing(14)} ${spacing(10)};
+    position: relative;
+    min-height: 215px;
+    border: 3px dashed;
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    align-self: stretch;
+    background-color: transparent;
+    border-color: ${colors[mode].border.border1};
+    outline-color: transparent;
+  `}
 `;
 
-interface EmptyPositionsProps {
-  isClosed?: boolean;
-}
+const EmptyPositions = () => {
+  const pushToHistory = usePushToHistory();
+  const dispatch = useAppDispatch();
 
-const EmptyPositions = ({ isClosed }: EmptyPositionsProps) => (
-  <StyledCard variant="outlined">
-    <Typography variant="h4">
-      <FormattedMessage
-        description="empty positions"
-        defaultMessage="No {status} positions yet."
-        values={{ status: isClosed ? 'closed' : 'open' }}
-      />
-    </Typography>
-  </StyledCard>
-);
+  const onClick = React.useCallback(() => {
+    dispatch(changeRoute(DCA_CREATE_ROUTE.key));
+    pushToHistory(`/${DCA_CREATE_ROUTE.key}`);
+  }, [dispatch, pushToHistory]);
+
+  return (
+    <StyledCard variant="outlined">
+      <ContainerBox flexGrow={1} justifyContent="center" alignItems="center" flexDirection="column" gap={6}>
+        <ContainerBox flexDirection="column" gap={2} alignItems="center" justifyContent="center">
+          <Typography variant="h5" fontWeight={500}>
+            <FormattedMessage description="createPositionPositionListTitle" defaultMessage="Balmy’s DCA" />
+          </Typography>
+          <Typography variant="bodyRegular">
+            <FormattedMessage
+              description="createPositionPositionListSubtitle"
+              defaultMessage="Spread your investment and earn yields."
+            />
+          </Typography>
+        </ContainerBox>
+        <Button variant="contained" color="primary" fullWidth onClick={onClick} size="large">
+          <FormattedMessage description="createPositionPositionList" defaultMessage="Create new position" />
+        </Button>
+      </ContainerBox>
+    </StyledCard>
+  );
+};
 
 export default EmptyPositions;
