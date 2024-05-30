e597f6f33484fab5f9a79e8e8cae55e4d9a117d9
th0r <144699868+th0rOdinson@users.noreply.github.com>
2024-05-30 14:05:36 -0300

    feat(earn): implement earnService (#828)

---
+++ packages/common-types/src/earn.ts
@@ -0,0 +1,45 @@
+import { Address } from 'viem';
+import { NetworkStruct, TokenWithIcon } from '.';
+
+export type ApiStrategy = {
+  id: string;
+  chainId: number;
+  asset: Address;
+  rewards: { token: Address; apy: number }[];
+  farm: ApiFarm;
+  guardian?: ApiGuardian;
+  // riskLevel?, // TBD
+};
+
+type ApiFarm = {
+  id: string;
+  name: string;
+  tvl: number;
+  yieldType: StrategyYieldType;
+};
+
+type ApiGuardian = {
+  name: string;
+  description: string;
+  fees: ApiGuardianFee[];
+};
+
+type ApiGuardianFee = {
+  type: 'deposit' | 'withdraw' | 'performance' | 'save';
+  percentage: number;
+};
+
+export enum StrategyYieldType {
+  LENDING,
+  STAKING,
+}
+
+export type Strategy = {
+  id: string;
+  asset: TokenWithIcon;
+  rewards: { token: TokenWithIcon; apy: number }[];
+  network: NetworkStruct;
+  yieldType: string;
+  farm: ApiFarm;
+  guardian?: ApiGuardian;
+};
