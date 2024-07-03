9b689121a09b61f0c99b5ebc7e705ad6faa4f30b
FiboApe <fiboape@gmail.com>
2024-07-03 13:49:32 -0300

    fix(providerservice): use sdk estimateGas method

--- apps/root/src/common/utils/wagmi.ts
+++ apps/root/src/common/utils/wagmi.ts
@@ -2,7 +2,27 @@ import { Connector } from 'wagmi';
 import { Config, getWalletClient } from '@wagmi/core';
 import { getProviderInfo } from './provider-info';
 import { Address } from '@types';
-import { WalletClient } from 'viem';
+import { WalletClient, TransactionRequest } from 'viem';
+
+export const viemTransactionTypeToSdkStype = (tx: TransactionRequest) => {
+  let type;
+  switch (tx.type) {
+    case 'legacy':
+      type = 0;
+      break;
+    case 'eip2930':
+      type = 1;
+      break;
+    case 'eip1559':
+      type = 2;
+      break;
+    case 'eip4844':
+      type = 3;
+      break;
+  }
+
+  return type;
+};
 
 export const getConnectorData = async (connector: Connector, config: Config) => {
   const walletClient = await getWalletClient(config, { connector });
