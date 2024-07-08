34e9476095d659880afd155241c42d18ef23e317
th0rOdinson <th0r@balmy.xyz>
2024-07-08 10:07:06 -0300

    Webpack improvements

--- apps/root/webpack.dev.js
+++ apps/root/webpack.dev.js
@@ -1,15 +1,22 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-    hot: true,
-    client: {
-      overlay: false,
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const smp = new SpeedMeasurePlugin();
+
+module.exports =
+  // smp.wrap({...
+  merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+      hot: true,
+      client: {
+        overlay: false,
+      },
     },
-  },
-});
+    // }),
+  });
