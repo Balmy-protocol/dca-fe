5e1d3ad0dfff8ca7dc0fb1b63a2b23fb275384ec
Sam Bugs <sambugs@protonmail.com>
2024-07-20 16:38:01 -0300

    feat: build fetchAllowancesInChain

--- apps/root/src/services/hacksLandingService.ts
+++ apps/root/src/services/hacksLandingService.ts
@@ -1,9 +1,10 @@
-import { Address, ChainId } from 'common-types';
+import { Address, ChainId, TokenAddress } from 'common-types';
+import { toLower } from '@balmy/sdk';
 import { EventsManager } from './eventsManager';
 import MeanApiService from './meanApiService';
 import { HackLanding, HackLandingId } from '@pages/hacks/types';
 import SdkService from './sdkService';
-import { erc20Abi, GetLogsReturnType } from 'viem';
+import { erc20Abi } from 'viem';
 
 export interface HacksLandingServiceData {
   hacksLandings: Record<HackLandingId, HackLanding>;
@@ -72,48 +73,77 @@ export default class HacksLandinService extends EventsManager<HacksLandingServic
     this.hacksLandings = hackLandings;
   }
 
-  async fetchWalletsAllowances(wallets: Address[], contracts: Record<ChainId, Address[]>) {
-    const promises: Promise<GetLogsReturnType | { failed: true; error: string }>[] = [];
+  fetchWalletsAllowances(
+    wallets: Address[],
+    contracts: Record<ChainId, Address[]>
+  ): Record<ChainId, Promise<Record<Wallet, Record<Contract, Record<TokenAddress, bigint>>>>> {
+    const entries = Object.entries(contracts).map(([chainId, contracts]) => [
+      chainId,
+      this.fetchAllowancesInChain(wallets, Number(chainId), contracts),
+    ]);
+    return Object.fromEntries(entries);
+  }
 
-    wallets.forEach((wallet) => {
-      Object.entries(contracts).forEach(([contractChainId, contractsAddresses]) => {
-        const provider = this.sdkService.sdk.providerService.getViemPublicClient({
-          chainId: Number(contractChainId),
-        });
+  private async fetchAllowancesInChain(wallets: Wallet[], chainId: ChainId, contracts: Contract[]) {
+    const provider = this.sdkService.sdk.providerService.getViemPublicClient({ chainId });
 
-        promises.push(
-          provider
-            .getBlockNumber()
-            .then((blockNumber) => {
-              const a = provider.getLogs({
-                event: erc20Abi[0],
-                args: {
-                  owner: wallet,
-                  spender: contractsAddresses,
-                },
-                fromBlock: 0n,
-                toBlock: blockNumber,
-              });
-
-              // eslint-disable-next-line promise/no-nesting
-              a.catch((e) => ({ failed: true, error: e as string }));
-
-              return a;
-            })
-            .catch((e) => ({ failed: true, error: e as string }))
-        );
-      });
+    // Fetch all logs for the wallets and contracts
+    const blockNumber = await provider.getBlockNumber();
+    const logs = await provider.getLogs({
+      event: erc20Abi[0],
+      args: {
+        owner: wallets,
+        spender: contracts,
+      },
+      fromBlock: 0n,
+      toBlock: blockNumber,
+      strict: true,
     });
 
-    const allowances = await Promise.all(promises);
-
-    allowances.forEach((allowance) => {
-      if ('failed' in allowance) {
-        console.log('Failed to fetch allowance', allowance.error);
-        return;
+    // Based on logs, determine which allowances to check for
+    const toCheck: Record<Lowercase<Wallet>, Record<Lowercase<Contract>, Set<Lowercase<TokenAddress>>>> = {};
+    for (const { address, args } of logs) {
+      const wallet = toLower(args.owner) as Lowercase<Wallet>;
+      const contract = toLower(args.spender) as Lowercase<Contract>;
+      const token = toLower(address) as Lowercase<TokenAddress>;
+      const value = args.value;
+      if (value > 0n) {
+        if (!toCheck[wallet]) toCheck[wallet] = {};
+        if (!toCheck[wallet][contract]) toCheck[wallet][contract] = new Set();
+        toCheck[wallet][contract].add(token);
+      } else {
+        // If the event was actually a revoke, then we can remove the token from the list
+        toCheck[wallet]?.[contract]?.delete(token);
       }
+    }
 
-      console.log('Got allowance', allowance);
+    // Fetch allowances
+    const toCheckArray = Object.entries(toCheck)
+      .flatMap(([wallet, record]) =>
+        Object.entries(record).flatMap(([contract, tokens]) =>
+          [...tokens].map((token) => ({ wallet, contract, token }))
+        )
+      )
+      .map(({ wallet, contract, token }) => ({ owner: wallet, spender: contract, token }));
+    const allowances = await this.sdkService.sdk.allowanceService.getAllowancesInChain({
+      chainId,
+      allowances: toCheckArray,
     });
+
+    // Switch to the format that return function expected
+    const result: Record<Wallet, Record<Contract, Record<TokenAddress, bigint>>> = {};
+    for (const { owner, spender, token: token_ } of toCheckArray) {
+      const wallet = owner as Wallet;
+      const contract = spender as Contract;
+      const token = token_ as TokenAddress;
+      if (!result[wallet]) result[wallet] = {};
+      if (!result[wallet][contract]) result[wallet][contract] = {};
+      result[wallet][contract][token] = allowances[token][owner][spender];
+    }
+
+    return result;
   }
 }
+
+type Wallet = Address;
+type Contract = Address;
