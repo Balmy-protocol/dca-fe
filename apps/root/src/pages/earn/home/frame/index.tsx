928c95c0ad1df6f22c831c1d39f68bbb2b49de4a
FiboApe <fiboape@gmail.com>
2024-09-05 00:21:14 -0300

    feat(earn): add earn title to all strategies page

--- apps/root/src/pages/earn/home/frame/index.tsx
+++ apps/root/src/pages/earn/home/frame/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ContainerBox, StyledNonFormContainer } from 'ui-library';
+import { colors, ContainerBox, StyledNonFormContainer, Typography } from 'ui-library';
 import EarnFAQ from '../../components/faq';
 import { useAppDispatch } from '@state/hooks';
 import { changeRoute } from '@state/tabs/actions';
@@ -8,6 +8,7 @@ import EarnWizard from '../components/wizard';
 import useEarnService from '@hooks/earn/useEarnService';
 import AllStrategiesTable from '../components/all-strategies-table';
 import useHasFetchedAllStrategies from '@hooks/earn/useHasFetchedAllStrategies';
+import { FormattedMessage } from 'react-intl';
 
 const EarnFrame = () => {
   const dispatch = useAppDispatch();
@@ -31,7 +32,20 @@ const EarnFrame = () => {
     <StyledNonFormContainer flexDirection="column" flexWrap="nowrap">
       <ContainerBox flexDirection="column" gap={32}>
         <ContainerBox flexDirection="column" gap={20}>
-          <EarnWizard />
+          <ContainerBox flexDirection="column" gap={5}>
+            <ContainerBox flexDirection="column" gap={2}>
+              <Typography variant="h1Bold" color={({ palette }) => colors[palette.mode].typography.typo1}>
+                <FormattedMessage defaultMessage="Earn" description="earn.all-strategies.title" />
+              </Typography>
+              <Typography variant="bodyLargeRegular" color={({ palette }) => colors[palette.mode].typography.typo3}>
+                <FormattedMessage
+                  defaultMessage="Invest in the best vaults!"
+                  description="earn.all-strategies.title-description"
+                />
+              </Typography>
+            </ContainerBox>
+            <EarnWizard />
+          </ContainerBox>
           <ContainerBox flex="1">
             <AllStrategiesTable />
           </ContainerBox>
