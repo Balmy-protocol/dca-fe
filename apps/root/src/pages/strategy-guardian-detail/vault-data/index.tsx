bfec53b03122bc71a415dc963072ebbaaa069b3d
FiboApe <fiboape@gmail.com>
2024-09-05 00:14:30 -0300

    feat(fee): add revised fee section

--- apps/root/src/pages/strategy-guardian-detail/vault-data/index.tsx
+++ apps/root/src/pages/strategy-guardian-detail/vault-data/index.tsx
@@ -5,7 +5,6 @@ import { BackgroundPaper, ContainerBox, DividerBorder2 } from 'ui-library';
 import DataHeader from './components/data-header';
 import DataCards from './components/data-cards';
 import DataAbout from './components/data-about';
-import DataHistoricalRate from './components/data-historical-rate';
 import DataGuardian from './components/data-guardian';
 
 interface VaultDataProps {
@@ -19,8 +18,6 @@ const StyledPaper = styled(BackgroundPaper).attrs({ variant: 'outlined' })`
 `;
 
 const VaultData = ({ strategy }: VaultDataProps) => {
-  const hasInvestment = !!strategy?.userPositions?.length;
-
   return (
     <StyledPaper>
       <ContainerBox flexDirection="column" alignItems="stretch" gap={6}>
@@ -28,8 +25,7 @@ const VaultData = ({ strategy }: VaultDataProps) => {
         <DividerBorder2 />
         <DataCards strategy={strategy} />
         {!!strategy?.guardian && <DataGuardian strategy={strategy} />}
-        <DataAbout strategy={strategy} collapsed={hasInvestment} />
-        {!hasInvestment && <DataHistoricalRate strategy={strategy} />}
+        <DataAbout strategy={strategy} collapsed />
       </ContainerBox>
     </StyledPaper>
   );
