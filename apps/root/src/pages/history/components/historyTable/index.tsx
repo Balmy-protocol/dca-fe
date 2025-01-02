4d6c58222ac262975e15c021d15f2bd06530baff
FiboApe <fiboape@gmail.com>
2025-01-02 18:47:00 -0300

    fixup! fix(history): fix usd values in table

--- apps/root/src/pages/history/components/historyTable/index.tsx
+++ apps/root/src/pages/history/components/historyTable/index.tsx
@@ -659,7 +659,6 @@ const HistoryTable = ({ search, tokens, height, solid }: HistoryTableProps) => {
       }, []);
     }, [unitsByChainPercentages]);
 
-  console.log(filteredEvents);
   return (
     <ContainerBox flexDirection="column" flex={1} gap={6}>
       {!isSomeWalletIndexed && !!wallets.length && hasLoadedEvents && (
