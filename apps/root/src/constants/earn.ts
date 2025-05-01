890e87b04b8c2c2b4843c7a74becf12421d639cf
FiboApe <fiboape@gmail.com>
2025-05-01 10:21:54 -0300

    feat(*): remove promoted from aave

--- apps/root/src/constants/earn.ts
+++ apps/root/src/constants/earn.ts
@@ -30,10 +30,10 @@ export const FEE_TYPE_STRING_MAP: Record<FeeType, ReturnType<typeof defineMessag
 // ];
 
 export const PROMOTED_STRATEGIES_IDS: StrategyId[] = [
-  '137-0xb034a43d1ffe0f88ed3a50fc096179f543fd3f3a-1',
-  '8453-0x020ebf53f4e5ef859e18e2973bd8d8b9af5c9c9f-2',
-  '8453-0x020ebf53f4e5ef859e18e2973bd8d8b9af5c9c9f-3',
-  '8453-0x020ebf53f4e5ef859e18e2973bd8d8b9af5c9c9f-4',
+  // '137-0xb034a43d1ffe0f88ed3a50fc096179f543fd3f3a-1',
+  // '8453-0x020ebf53f4e5ef859e18e2973bd8d8b9af5c9c9f-2',
+  // '8453-0x020ebf53f4e5ef859e18e2973bd8d8b9af5c9c9f-3',
+  // '8453-0x020ebf53f4e5ef859e18e2973bd8d8b9af5c9c9f-4',
 ];
 
 export const STRATEGIES_WITH_LM_REWARDS: StrategyId[] = [
@@ -41,6 +41,6 @@ export const STRATEGIES_WITH_LM_REWARDS: StrategyId[] = [
   // '8453-0xc0571929c21b71fc7579ec7159b1e88a2199bc78-16',
   // '8453-0xc0571929c21b71fc7579ec7159b1e88a2199bc78-17',
   // '8453-0xc0571929c21b71fc7579ec7159b1e88a2199bc78-18',
-  '8453-0xc0571929c21b71fc7579ec7159b1e88a2199bc78-20',
-  '8453-0xc0571929c21b71fc7579ec7159b1e88a2199bc78-21',
+  // '8453-0xc0571929c21b71fc7579ec7159b1e88a2199bc78-20',
+  // '8453-0xc0571929c21b71fc7579ec7159b1e88a2199bc78-21',
 ];
